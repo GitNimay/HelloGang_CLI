195489519ebc426018d3c5293c1eeeb884646aeb
GitNimay <nimesh.kulkarni2004@gmail.com>
2026-03-24 19:48:55 +0530

    fix: add non-Windows stubs for startup app functions

--- internal/install/install_other.go
+++ internal/install/install_other.go
@@ -13,3 +13,13 @@ func installCMD(opts InstallOptions) error {
 func uninstallCMD(opts InstallOptions) error {
 	return fmt.Errorf("CMD AutoRun is only supported on Windows")
 }
+
+// InstallStartupApp is not supported on non-Windows platforms
+func InstallStartupApp(opts InstallOptions) error {
+	return fmt.Errorf("Windows Startup App is only supported on Windows")
+}
+
+// UninstallStartupApp is not supported on non-Windows platforms
+func UninstallStartupApp(opts InstallOptions) error {
+	return fmt.Errorf("Windows Startup App is only supported on Windows")
+}
